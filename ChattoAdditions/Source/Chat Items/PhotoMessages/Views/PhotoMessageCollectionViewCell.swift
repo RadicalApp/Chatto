ad1beadebf9e376825240f6c6f1dbb6505ad2056
Sameer Khavanekar <sameer@cyberdust.com>
2018-04-15 01:08:23 -0700

    To override outside framework

--- ChattoAdditions/Source/Chat Items/PhotoMessages/Views/PhotoMessageCollectionViewCell.swift
+++ ChattoAdditions/Source/Chat Items/PhotoMessages/Views/PhotoMessageCollectionViewCell.swift
@@ -28,7 +28,7 @@ public typealias PhotoMessageCollectionViewCellStyleProtocol = PhotoBubbleViewSt
 
 public final class PhotoMessageCollectionViewCell: BaseMessageCollectionViewCell<PhotoBubbleView> {
 
-    static func sizingCell() -> PhotoMessageCollectionViewCell {
+    public static func sizingCell() -> PhotoMessageCollectionViewCell {
         let cell = PhotoMessageCollectionViewCell(frame: CGRect.zero)
         cell.viewContext = .sizing
         return cell
@@ -44,7 +44,7 @@ public final class PhotoMessageCollectionViewCell: BaseMessageCollectionViewCell
         }
     }
 
-    var photoMessageViewModel: PhotoMessageViewModelProtocol! {
+    public var photoMessageViewModel: PhotoMessageViewModelProtocol! {
         didSet {
             self.messageViewModel = self.photoMessageViewModel
             self.bubbleView.photoMessageViewModel = self.photoMessageViewModel
